e60799b39cb56f52460daa7692aaee11a24d5c42
Pamilerin Oyewole <pamilerinoyewole06@gmail.com>
2024-02-17 13:09:32 +0100

    updated project and aboit pages

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,29 @@
+import pro1 from "../assets/project1.png";
+import pro2 from "../assets/project2.png";
+import pro3 from "../assets/project3.png";
+
+const ProjectCardData = [
+  {
+    imgsrc: pro1,
+    title: "Ecommerce Website Design",
+    text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tanfible and self-affirming sense of \"Wow! I really can do this\" for over 100,000 online entrepreneurs. Best-selling marketing courses",
+    view: "https://www.youtube.com",
+    source: "google.com"
+  },
+  {
+    imgsrc: pro2,
+    title: "LMS/Course Website Design",
+    text: "Podcast Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tanfible and self-affirming sense of \"Wow! I really can do this\" for over 100,000 online entrepreneurs. Best-selling marketing courses",
+    view: "https://www.youtube.com",
+    source: "google.com"
+  },
+  {
+    imgsrc: pro3,
+    title: "Rymo Ecommerce Design",
+    text: "Top-Ranked Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tanfible and self-affirming sense of \"Wow! I really can do this\" for over 100,000 online entrepreneurs. Best-selling marketing courses",
+    view: "https://www.youtube.com",
+    source: "google.com"
+  }
+]
+
+export default ProjectCardData;
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from "../components/Footer";
 import HeroImg2 from '../components/HeroImg2';
+import PricingCard from "../components/PricingCard";
+import Work from "../components/Work";
 
 
 const Project = () => {
@@ -10,6 +12,8 @@ const Project = () => {
     <div>
       <Navbar/>
       <HeroImg2 heading="PROJECTS." text = "Some of my most recent works"/>
+      <Work/>
+      <PricingCard/>
       <Footer/>
     </div>
   )
